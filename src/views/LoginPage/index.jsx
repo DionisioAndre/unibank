6768c031477c6853fd4f9a6850dbd501b4699f6d
DionisioAndre <92489545+DionisioAndre@users.noreply.github.com>
2024-11-10 08:34:52 +0100

    Update index.jsx

--- src/views/LoginPage/index.jsx
+++ src/views/LoginPage/index.jsx
@@ -39,7 +39,7 @@ function Loginpage() {
                                         <div className="card-body p-4 p-lg-5 text-black">
                                             <form onSubmit={handlesubmit}>
                                                 <div className="d-flex align-items-center mb-3 pb-1">
-                                                    <span className="h2 fw-bold mb-0">Bem vindo a <b>Unibank</b></span>
+                                                    <span className="h2 fw-bold mb-0">Bem vindo a <b>Kitadibue</b></span>
                                                 </div>
                                                 <h5 className="fw-normal mb-3 pb-3" style={{ letterSpacing: 1 }}>Entrar</h5>
                                                 <div className="form-outline mb-4">
